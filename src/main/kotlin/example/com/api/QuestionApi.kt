1fd7ea59d13936a223c8456f34fc5e53b4552704
Delly <daehyeon1999@naver.com>
2024-07-02 09:51:05 +0900

    Add: 🏷️ Question 라우팅 추가

--- src/main/kotlin/example/com/api/QuestionApi.kt
+++ src/main/kotlin/example/com/api/QuestionApi.kt
@@ -1,6 +1,19 @@
 package example.com.api
 
+import io.github.smiley4.ktorswaggerui.dsl.resources.get
 import io.ktor.resources.*
+import io.ktor.server.routing.*
 
-@Resource("/question")
-class QuestionApi
+@Resource("/questions")
+class QuestionApi {
+
+  @Resource("/fetch")
+  class Fetch
+}
+
+fun Route.question() {
+
+  get<QuestionApi>({}) {}
+
+  get<QuestionApi.Fetch>({}) {}
+}
--- src/main/kotlin/example/com/plugins/Routing.kt
+++ src/main/kotlin/example/com/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package example.com.plugins
 
+import example.com.api.question
 import io.ktor.server.application.*
 import io.ktor.server.resources.*
 import io.ktor.server.routing.*
@@ -8,4 +9,9 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
   install(Routing)
   install(Resources)
+
+
+  routing {
+    question()
+  }
 }
